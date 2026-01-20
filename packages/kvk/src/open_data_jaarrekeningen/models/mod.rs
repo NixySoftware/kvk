b2711e6048e535a3ce6ddb183b0fdfc950bf6924
Daniëlle Huisman <danielle@nixy.software>
2026-01-20 11:44:16 +0100

    feat: add client generation (#1)

---
+++ packages/kvk/src/open_data_jaarrekeningen/models/mod.rs
@@ -0,0 +1,8 @@
+pub mod error;
+pub use self::error::Error;
+pub mod melding;
+pub use self::melding::Melding;
+pub mod opendata;
+pub use self::opendata::Opendata;
+pub mod opendata_field;
+pub use self::opendata_field::OpendataField;
